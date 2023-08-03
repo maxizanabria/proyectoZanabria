34805ee4ba96117b76d5f7cfbaa6b854797055ac
Maxi Zanabria <maxi.zanabria@hotmail.com>
2023-08-03 19:17:14 -0300

    first commit

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import App from '../App'
+
+
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+      <h1>
+        cara de verga
+
+      </h1>
+       
+    </div>
+
+
+
+
+    </>
+  )
+}
+
+export default Navbar
