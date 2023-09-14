db3279f0f8d4bde903f0910b788c9859b05b7123
Maxi Zanabria <maxi.zanabria@hotmail.com>
2023-09-13 22:07:58 -0300

    entregaFinal3

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom'
 
 function Item({ producto }) {
     return (
-        <div>
+        <div className='producto'>
+            <img src={producto.imagen} alt={producto.titulo} />
             <h3>{producto.titulo}</h3>
             <p>Precio: ${producto.precio}</p>
             <p>Categoria: {producto.categoria}</p>
-            <p>{producto.descripcion}</p>
-            <img src={producto.imagen} alt={producto.titulo} />
-            <Link className='ver-mas' to={`/item/${producto.id}`}> Detalle </Link>
+            <Link className='ver-mas' to={`/item/${producto.id}`}> Ver medicamento </Link>
 
         </div>
     )
